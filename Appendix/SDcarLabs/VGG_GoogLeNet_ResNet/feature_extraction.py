e4fadb49d26f8d107cd5d07d3b1433a7c5c3877a
Arturo PS <arturo.ps14@gmail.com>
2017-09-19 23:00:14 -0400

    Finished the transfer learning - feature extraction in cifar 10, traffic signs using vgg, googlenet and resnet

--- Appendix/SDcarLabs/VGG_GoogLeNet_ResNet/feature_extraction.py
+++ Appendix/SDcarLabs/VGG_GoogLeNet_ResNet/feature_extraction.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 # TODO: import Keras layers you need here
 import numpy as np
 from keras.layers import Input, Flatten, Dense
-from keras.model import Model
+from keras.models import Model
 
 flags = tf.app.flags
 FLAGS = flags.FLAGS
@@ -11,7 +11,8 @@ FLAGS = flags.FLAGS
 # command line flags
 flags.DEFINE_string('training_file', '', "Bottleneck features training file (.p)")
 flags.DEFINE_string('validation_file', '', "Bottleneck features validation file (.p)")
-
+flags.DEFINE_integer('epochs', 50, "Number of epochs")
+flags.DEFINE_integer('batch_size', 256, "Batch size")
 
 def load_bottleneck_data(training_file, validation_file):
     """
@@ -49,9 +50,18 @@ def main(_):
     # the dataset
     # 10 for cifar10
     # 43 for traffic
+    num_classes = len(np.unique(y_train))
 
-    # TODO: train your model here
+    # model definition
+    input_shape = X_train.shape[1:]
+    cache_input = Input(shape=input_shape)
+    flat = Flatten()(cache_input)
+    fc = Dense(num_classes, activation='softmax')(flat)
+    model = Model(cache_input,fc)
+    model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
+    # train model
+    model.fit(X_train, y_train, FLAGS.batch_size , FLAGS.epochs, validation_data=(X_val, y_val), shuffle=True)
 
 # parses flags and calls the `main` function above
 if __name__ == '__main__':
